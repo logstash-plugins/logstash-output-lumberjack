3668372f24d7923f44a455e3dff8ddd0ac3c7bbb
andsel <selva.andre@gmail.com>
2021-02-10 10:24:36 +0100

    In Logstash 8.0 the SHUTDOWN event is removed and was deprecated/not used since Logstash 5.0, remove it.

--- lib/logstash/outputs/lumberjack.rb
+++ lib/logstash/outputs/lumberjack.rb
@@ -55,7 +55,6 @@ class LogStash::Outputs::Lumberjack < LogStash::Outputs::Base
 
   public
   def receive(event)
-    return if event == LogStash::SHUTDOWN
     @codec.encode(event)
   end # def receive
 
